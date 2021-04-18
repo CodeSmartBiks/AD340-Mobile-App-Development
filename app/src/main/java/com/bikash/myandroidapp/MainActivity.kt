de090417950a8fb12843efee135469a14145f905
Biks <bikasjthapa@gmail.com>
2021-04-18 12:57:43 -0700

    HW-1 UILayout

--- app/src/main/java/com/bikash/myandroidapp/MainActivity.kt
+++ app/src/main/java/com/bikash/myandroidapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.bikash.myandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View;
+import android.widget.Toast;
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+    val btnArray = arrayOf("Send", "ZipOne", "ZipTwo", "ZipThree", "ZipFour")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun showText(view: View) {
+        //val button: Button = findViewById(R.id.gButton1)
+        val button = view as Button
+        val context = applicationContext
+        val duration = Toast.LENGTH_SHORT
+        val text = button.text
+
+        val btnText = Toast.makeText(context, text, duration).show()
+    }
+
+
 }
